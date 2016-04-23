b7ab13aa031a07195919aa7e541aeea3da6f3379
RogerPu <espwj@126.com>
2016-04-24 02:33:11 +0800

    pycurl resp code

---
+++ HTTP_Monitor/3pycurl-examing-response.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+#Auth RogerPuX Plateno Groups Inc.
+import pycurl
+try:
+	from io import BytesIO
+except ImportError:
+	from StringIO import StringIO as BytesIO
+
+buffer = BytesIO()
+c = pycurl.Curl()
+#c.setopt(c.URL,'10.21.250.46/rest/couponServices?_wadl')
+c.setopt(c.URL,'plateno.com')
+c.setopt(c.FOLLOWLOCATION,True)
+c.setopt(c.WRITEDATA,buffer)
+c.perform()
+
+# HTTP response code, e.g. 200.
+print('status: %d' % c.getinfo(c.RESPONSE_CODE))
+# Elapsed time for the transfer.
+print('status: %d' % c.getinfo(c.TOTAL_TIME))
+
+
+# getinfo must be called before close.
+c.close()
