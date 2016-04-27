92babff9e6fb8da5d0f7929d7f130224212e0997
RogerPu <espwj@126.com>
2016-04-28 00:18:19 +0800

    update

--- Python_Scripts/multi_task_base_gateway_server.py
+++ Python_Scripts/multi_task_base_gateway_server.py
@@ -2,39 +2,55 @@
 import pexpect
 import sys
 
+# ip = '10.100.112.88'
+# user = 'roger'
+# cmd = '/sbin/ip a'
+
 ip = sys.argv[1]
 user = sys.argv[2]
 cmd = sys.argv[3]
 
 def operation(ip,user,cmd):
 
-    cmd_combie = 'ssh ' + user +'@' + ip
-
-    child = pexpect.spawn(cmd_combie,timeout=2)
-    i = child.expect(["password:",pexpect.TIMEOUT])
-    if i == 0:
+    cmd_combine = '/usr/bin/ssh ' + user +'@' + ip + ' "' + cmd + '"'
+    #print cmd_combine
+    child = pexpect.spawn(cmd_combine,timeout=2)
+    index = child.expect(["password:",pexpect.TIMEOUT,pexpect.EOF])
+    if index == 0:
         child.kill(0)
-    elif i == 1:
+        r = 'passed without passwd'
+        return (r,cmd_combine)
+    elif index == 1:
         child.kill(0)
+        r = 'passed cause timeout'
+        return (r,cmd_combine)
+    elif index == 2:
+        child = pexpect.spawn(cmd_combine,timeout=2)
+        r = child.read()
+        #print r
+        return (r,cmd_combine)
+    #    child.kill(0)
+    # else:
+    #     r = child.read()
+    #     #print r
+    #     return (r,cmd_combine)
 
-    cmd_final = cmd_combie + " " + '"' + cmd + '"'
-    child = pexpect.spawn(cmd_final)
-    r = child.read()
-    #print r
-    return (r,cmd_final)
+# operation(ip,user,cmd)
 
+def multi_command(ip,user,cmd1,cmd2="free -m"):
 
-def multi_command(ip,user,cmd1,cmd2):
-
-    (r2,r2_cmd_final) = operation(ip,user,cmd1)
-    print r2_cmd_final
+    (r2,r2_cmd_combine) = operation(ip,user,cmd1)
+    print r2_cmd_combine
     print r2
-    if r2.find("redis-server"):
-        (r3,r3_cmd_final) = operation(ip,user,cmd2)
-        print r3_cmd_final
+    if r2.find('redis-server') != -1 :
+        (r3,r3_cmd_combine) = operation(ip,user,cmd2)
+        print r3_cmd_combine
         print r3
-
-multi_command(ip,user,cmd,cmd2="free -m")
+        return r3
+    else:
+        print('Cannot find redis-server')
+        print('Cannot find redis-server')
+multi_command(ip,user,cmd)
 
 #if __main___ == "__main__":
 #    multi_command()
